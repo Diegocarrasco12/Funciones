ac92ac7f53b2e8f2e0b664c8ee64850433650dfd
Diego Carrasco <diegocarrasco.ordonez@gmail.com>
2024-06-14 16:33:51 -0400

    pba

---
+++ 1 funcion.js
@@ -0,0 +1,8 @@
+//ejemplo desafío:  
+//function example(a, b, c){
+ //return a+b+c //
+
+ //Declaración de función a función de expresión//
+const example = (a, b, c) => {
+    return a + b + c
+}
---
+++ 2 arrow.js
@@ -0,0 +1,6 @@
+//Ejemplo desafío:  let suma = function(a, b){
+ //return a + b
+//}
+
+//Transformación a Arrow de una línea//
+const suma = (a, b) => a + b
